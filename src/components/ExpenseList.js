5333ba34e221c2f6be992ee6186fe476856866f6
Rishu Sahu <rishusahu23@gmail.con>
2020-06-08 13:57:42 +0530

    added amount

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -2,12 +2,11 @@ import React from 'react'
 import {connect} from 'react-redux'
 import ExpenseListItem from './ExpenseListItem'
 import selectExpenses from '../selectors/expenses'
-import ExpenseListFilters from './ExpenseListFilters'
 
 const ExpenseList=(props)=>{
     return(
         <div>
-            <ExpenseListFilters />
+           
             <h2>Expense List</h2>
             {
                 props.expenses.map((expense)=><ExpenseListItem key={expense.id} {...expense} />)
