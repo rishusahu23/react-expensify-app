a08beaafc6a7acaaf37227071d1b3a426b951842
Rishu Sahu <rishusahu23@gmail.con>
2020-06-08 18:25:57 +0530

    development environment

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,14 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { applyMiddleware,createStore, combineReducers,compose } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import thunk  from 'redux-thunk'
+
+const composeEnhancers=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__||compose
 
 export default () => {
   const store = createStore(
     combineReducers({
       expenses: expensesReducer,
       filters: filtersReducer
-    })
+    }),
+    composeEnhancers(applyMiddleware(thunk))
+   
   );
 
   return store;
 };
+ //window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
