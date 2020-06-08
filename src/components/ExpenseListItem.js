f648c799d86aa953f0ca804cb0ae36b942c074cc
Rishu Sahu <rishusahu23@gmail.con>
2020-06-08 12:28:34 +0530

    formatting for amount

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {NavLink} from 'react-router-dom'
-
+import moment from 'moment'
+import numeral from 'numeral'
 
 
 const ExpenseListItem =({id,amount,description,createdAt,note})=>{
    // const {id,amount,description,createdAt,note}=props.expense
+  // numeral.locale('Rs')
+   
         return(
         <div>
             <NavLink to={`/edit/${id}`} exact={true} activeClassName="is-active">
                 {description}
             </NavLink>
             
-            <h4>{amount} - {createdAt} - {note}</h4>
+            <h4>
+                {
+                   
+                    numeral(amount/100).format('Rs0,0.00')}
+                -  
+                {moment(createdAt).format('MMMM Do, YYYY')} 
+                - 
+                {note}</h4>
            
         </div>
     )
